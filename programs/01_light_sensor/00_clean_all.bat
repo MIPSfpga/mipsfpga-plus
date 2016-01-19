f209f7ed027bf2c3db8c8a541e5a4a30172ee8bf
Yuri Panchul <yuri@panchul.com>
2016-01-18 17:30:35 -0800

    Updated other examples for consistency with counter

--- programs/01_light_sensor/00_clean_all.bat
+++ programs/01_light_sensor/00_clean_all.bat
@@ -5,6 +5,6 @@ del main.s
 del program.elf
 del program.map
 del program.dis
-del program.hex
+del program*.hex
 del program.rec
 del FPGA_Ram.elf
--- programs/01_light_sensor/12_upload_to_the_board_using_uart.bat
+++ programs/01_light_sensor/12_upload_to_the_board_using_uart.bat
@@ -1,3 +1,3 @@
-set a=33
+set a=16
 mode com%a% baud=115200 parity=n data=8 stop=1 to=off xon=off odsr=off octs=off dtr=off rts=off idsr=off
 type program.rec >\\.\COM%a%
