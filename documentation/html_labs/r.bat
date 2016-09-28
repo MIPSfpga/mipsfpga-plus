9ae25fc121a15766c33c4581214ad7c844776adf
Yuri Panchul <yuri@panchul.com>
2016-09-28 14:58:49 -0700

    Next step in documentation

--- documentation/html_labs/r.bat
+++ documentation/html_labs/r.bat
@@ -1,1 +1,1 @@
-copy lab1_0.html+lab1_1.html+lab1_2.html+lab1_3.html+lab1_4.html+lab1_5.html+lab1_a.html+lab1_z.html lab1.html
+copy lab1_0.html+lab1_1.html+lab1_2.html+lab1_3.html+lab1_4_1.html+lab1_4_2.html+lab1_4_3.html+lab1_5.html+lab1_a.html+lab1_z.html lab1.html
