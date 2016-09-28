39984fbb8b4b6c1c805658412165e3a50a88d6a8
Yuri Panchul <yuri@panchul.com>
2016-09-28 12:39:39 -0700

    Started writing Serial Loader Flow Lab document

--- programs/00_counter/12_upload_to_the_board_using_uart.bat
+++ programs/00_counter/12_upload_to_the_board_using_uart.bat
@@ -1,7 +1,4 @@
-set a=15
-set a=38
 set a=16
-set a=34
-set a=44
+set a=39
 mode com%a% baud=115200 parity=n data=8 stop=1 to=off xon=off odsr=off octs=off dtr=off rts=off idsr=off
 type program.rec >\\.\COM%a%
