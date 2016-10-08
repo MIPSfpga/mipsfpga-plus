c39120d50f150e4103c9c12617fbe8fc5d4c5016
yuri.panchul <yuri@panchul.com>
2016-10-07 18:55:21 -0700

    Renamed and moved around lab_yp1 files

--- documentation/html_labs/r.bat
+++
@@ -1,1 +0,0 @@
-copy lab1_0.html+lab1_1.html+lab1_2.html+lab1_3.html+lab1_4_1.html+lab1_4_2.html+lab1_4_3.html+lab1_5_1_5_4.html+lab1_5_5_1.html+lab1_5_5_2.html+lab1_5_5_3.html+lab1_5_6.html+lab1_a.html+lab1_z.html lab1.html
