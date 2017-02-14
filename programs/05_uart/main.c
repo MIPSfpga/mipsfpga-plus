cee8768ea8b7dcac6d42ca129e2ee9eb69c05c95
Stanislav Zhelnio <zhelniosl@yandex.ru>
2017-02-14 22:38:25 +0300

    uart program refactoring

--- programs/05_uart/main.c
+++ programs/05_uart/main.c
@@ -8,25 +8,42 @@ void _delay(uint32_t val)
         __asm__ volatile("nop");
 }
 
-int main ()
+void uartInit(uint16_t divisor)
 {
-    //uart init
     MFP_UART_LCR = MFP_UART_LCR_8N1;                    // 8n1
     MFP_UART_MCR = MFP_UART_MCR_DTR | MFP_UART_MCR_RTS; // DTR + RTS
 
     MFP_UART_LCR |= MFP_UART_LCR_LATCH;   // Divisor Latches access enable
-    MFP_UART_DLL = 1;                     // Divisor LSB
-    MFP_UART_DLH = 0;                     // Divisor MSB
+    MFP_UART_DLL = divisor & 0xFF;        // Divisor LSB
+    MFP_UART_DLH = (divisor >> 8) & 0xff; // Divisor MSB
     MFP_UART_LCR &= ~MFP_UART_LCR_LATCH;  // Divisor Latches access disable
+}
 
-    //uart transmit
-    MFP_UART_TXR = 0x11;        // transmitted data
+void uartTransmit(uint8_t data)
+{
+    MFP_UART_TXR = data;        // transmitted data
+}
 
+uint8_t uartReceive(void)
+{
     //waiting for RX data
     while (!(MFP_UART_LSR & MFP_UART_LSR_DR));
+    //returning received data
+    return MFP_UART_RXR;
+}
+
+int main ()
+{
+    const uint16_t uartDivisor = 1;
+
+    //uart init
+    uartInit(uartDivisor);
+
+    //uart transmit
+    uartTransmit(0x11);
 
     //received data output
-    MFP_RED_LEDS = MFP_UART_RXR;
+    MFP_RED_LEDS = uartReceive();
 
     for(;;);
 }
