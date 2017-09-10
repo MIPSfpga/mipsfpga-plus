55a6f17a348ba60c4fffeeca0c84dbc102dd1003
Stanislav Zhelnio <zhelniosl@yandex.ru>
2017-09-10 23:27:52 +0300

    readme and script update

--- scripts/init_clear.sh
+++ scripts/init_clear.sh
@@ -4,14 +4,6 @@ SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
 TOP_BOARD=$SCRIPTPATH/../boards
 TOP_PROGRAM=$SCRIPTPATH/../programs
 
-for board in $TOP_BOARD/*
-do
-    if [ -d $board ] && [ "$TOP_BOARD/program" != "$board" ]; then
-	rm -f $board/*.bat
-    rm -f $board/*.sh
-    fi
-done
-
 for program in $TOP_PROGRAM/*
 do
     if [ -d $program ]; then
