60720049776b512b27b152ac4894144c780218b3
Stanislav Zhelnio <S.Zhelnio@iva-tech.ru>
2019-02-01 20:57:25 +0300

    message about the 'make' usage added to all make_project scripts

--- boards/de0_nano/make_project.sh
+++ boards/de0_nano/make_project.sh
@@ -8,3 +8,5 @@ mkdir $DIRNAME
 cp *.qpf $DIRNAME
 cp *.qsf $DIRNAME
 cp *.sdc $DIRNAME
+
+echo "Project created. You can also use a 'make' tool to build it and program FPGA chip in console mode. Run 'make help' for detailes"
