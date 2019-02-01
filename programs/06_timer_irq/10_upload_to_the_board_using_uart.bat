4700da1ad6b786100474247660297eafc8a9f7ae
Stanislav Zhelnio <zhelniosl@yandex.ru>
2019-02-01 21:49:30 +0300

    windows *.bat replaced with its *.sh alternatives in program dir

--- programs/06_timer_irq/10_upload_to_the_board_using_uart.bat
+++
@@ -1,4 +0,0 @@
-set a=16
-
-mode com%a% baud=115200 parity=n data=8 stop=1 to=off xon=off odsr=off octs=off dtr=off rts=off idsr=off
-type program.rec >\\.\COM%a%
--- programs/09_adc/07_simulate_with_icarus.bat
+++
@@ -1,2 +0,0 @@
-make icarus
-make gtkwave
