a1740ab41cf8bf2062d164853cd088cbaf071a66
Yuri Panchul <yuri@panchul.com>
2019-05-13 07:24:47 +0300

    Added hardcoded program that runs before uart loader

--- system_rtl/mfp_ahb_ram_slave.v
+++ system_rtl/mfp_ahb_ram_slave.v
@@ -31,6 +31,8 @@ module mfp_ahb_ram_slave
 
     assign HRESP  = 1'b0;
 
+    wire [31:0] HRDATA_from_ram;
+
     mfp_ahb_lite_slave 
     #(
         .ADDR_WIDTH ( ADDR_WIDTH ),
@@ -67,12 +69,12 @@ module mfp_ahb_ram_slave
                 )
                 ram
                 (
-                    .clk          ( HCLK                      ),
-                    .read_addr    ( read_addr                 ),
-                    .write_addr   ( write_addr                ),
-                    .write_data   ( HWDATA     [ i * 8 +: 8 ] ),
-                    .write_enable ( write_mask [ i ]          ),
-                    .read_data    ( HRDATA     [ i * 8 +: 8 ] )
+                    .clk          ( HCLK                           ),
+                    .read_addr    ( read_addr                      ),
+                    .write_addr   ( write_addr                     ),
+                    .write_data   ( HWDATA          [ i * 8 +: 8 ] ),
+                    .write_enable ( write_mask      [ i ]          ),
+                    .read_data    ( HRDATA_from_ram [ i * 8 +: 8 ] )
                 );
             end
         endgenerate
@@ -90,8 +92,65 @@ module mfp_ahb_ram_slave
             .write_addr   ( write_addr      ),
             .write_data   ( HWDATA          ),
             .write_enable ( write_enable    ),
-            .read_data    ( HRDATA          )
+            .read_data    ( HRDATA_from_ram )
         );
+    `endif
+
+    `ifdef SIMULATION1
+
+        assign HRDATA = HRDATA_from_ram;
+
+    `elsif MFP_INITIALIZE_MEMORY_FROM_TXT_FILE1
+
+        assign HRDATA = HRDATA_from_ram;
+
+    `else
+
+        // We check if ram was ever written.
+        // If not, we return hardcoded program.
+
+        reg ram_was_ever_written;
+
+        always @ (posedge HCLK or negedge HRESETn)
+            if (! HRESETn)
+                ram_was_ever_written <= 1'b0;
+            else if (write_enable)
+                ram_was_ever_written <= 1'b1;
+
+        reg [31:0] HRDATA_hardcoded;
+
+        always @*
+            case (HADDR)
+            32'h1fc00000 : HRDATA_hardcoded = 32'h00001825;  // move  v1,zero
+            32'h1fc00004 : HRDATA_hardcoded = 32'h00002825;  // move  a1,zero
+            32'h1fc00008 : HRDATA_hardcoded = 32'h3c04bf80;  // lui   a0,0xbf80
+            32'h1fc0000c : HRDATA_hardcoded = 32'h00053600;  // sll   a2,a1,0x18
+            32'h1fc00010 : HRDATA_hardcoded = 32'h00031202;  // srl   v0,v1,0x8
+            32'h1fc00014 : HRDATA_hardcoded = 32'h00c21025;  // or    v0,a2,v0
+            32'h1fc00018 : HRDATA_hardcoded = 32'h7c023804;  // ins   v0,zero,0x0,0x8
+            32'h1fc0001c : HRDATA_hardcoded = 32'h306600ff;  // andi  a2,v1,0xff
+            32'h1fc00020 : HRDATA_hardcoded = 32'h00461025;  // or    v0,v0,a2
+            32'h1fc00024 : HRDATA_hardcoded = 32'h00023202;  // srl   a2,v0,0x8
+            32'h1fc00028 : HRDATA_hardcoded = 32'hac860000;  // sw    a2,0(a0)
+            32'h1fc0002c : HRDATA_hardcoded = 32'h8c860000;  // lw    a2,0(a0)
+            32'h1fc00030 : HRDATA_hardcoded = 32'hac860004;  // sw    a2,4(a0)
+            32'h1fc00034 : HRDATA_hardcoded = 32'hac820010;  // sw    v0,16(a0)
+            32'h1fc00038 : HRDATA_hardcoded = 32'h24620001;  // addiu v0,v1,1
+            32'h1fc0003c : HRDATA_hardcoded = 32'h0043302b;  // sltu  a2,v0,v1
+            32'h1fc00040 : HRDATA_hardcoded = 32'h00401825;  // move  v1,v0
+            32'h1fc00044 : HRDATA_hardcoded = 32'h1000fff1;  // b     0x1fc0000c
+            32'h1fc00048 : HRDATA_hardcoded = 32'h00c52821;  // addu  a1,a2,a1
+            default      : HRDATA_hardcoded = 32'h00000000;
+            endcase
+
+        reg [31:0] HRDATA_hardcoded_reg;
+
+        always @ (posedge HCLK)
+            HRDATA_hardcoded_reg <= HRDATA_hardcoded;
+
+        assign HRDATA = ram_was_ever_written ?
+            HRDATA_from_ram : HRDATA_hardcoded_reg;
+
     `endif
 
 endmodule
