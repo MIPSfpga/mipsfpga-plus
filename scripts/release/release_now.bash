3c9f7c698894c881fb9be41e7e3349ff9728f9ad
Yuri Panchul <yuri@panchul.com>
2019-05-31 11:57:30 -0700

    Added release script

---
+++ scripts/release/release_now.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+rm -rf   ~/mipsopen_ssd
+mkdir -p ~/mipsopen_ssd
+mkdir -p ~/mipsopen_ssd/mipsopen
+cp -r ../../.* ../../* ~/mipsopen_ssd/mipsopen
+cd ~/mipsopen_ssd/mipsopen
+
+rm -rf \
+./.git \
+./documentation/html_to_imgtec_forum_converter \
+./documentation/html_labs \
+./documentation/connecting_instructions \
+./documentation/slides/MIPS Open Introduction - 4JUNE2019 v1.0.pptx \
+./documentation/html_labs_mips_open \
+./documentation/.gitignore \
+./documentation/html_labs_in_russian \
+./documentation/edited_figures \
+./documentation/figure_generator \
+./documentation/html_labs_for_blog \
+./scripts/release \
+./.gitignore \
+./programs/12_linux/.gitignore \
+./documentation/.gitignore \
+./core/.gitignore \
+
