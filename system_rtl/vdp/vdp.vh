e199974c3f45e075617c0ffbbba213e041524c3a
Yuri Panchul <yuri@panchul.com>
2019-06-14 22:57:10 +0300

    Developing VDP

--- system_rtl/vdp/vdp.vh
+++ system_rtl/vdp/vdp.vh
@@ -6,22 +6,36 @@
 //     | ||||
 //     | |||| +------------------------------      23 sprite indicator
 //     | |||| |
-//     | |||| |                     +--------       5 sprite coordinates
-//     | |||| |                     |
-//     | |||| |                     |+-++----  4 :  2 sprite line
-//     | |||| |                     || ||
-//     V VVVV V                     VV VV
+//     | |||| |                 + ++---------  8 :  6 sprite number
+//     | |||| |                 | ||
+//     | |||| |                 | ||+--------       5 sprite coordinates
+//     | |||| |                 | |||
+//     | |||| |                 | |||+-++----  4 :  2 sprite row
+//     | |||| |                 | |||| ||
+//     V VVVV V                 V VVVV VV
 //
 //  3322 2222 2222 1111 1111 1100 0000 0000
 //  1098 7654 3210 9876 5432 1098 7654 3210
 //
-//  ...1 1110 I... .... .... .... ..CL LL..
+//  ...1 1110 S... .... .... .... ..CR RR..
 
-`define VDP_ADDR_SPRITE_BIT_INDICATOR        23
-`define VDP_ADDR_SPRITE_BIT_COORDINATES       5
-`define VDP_ADDR_SPRITE_RANGE_LINE        4 : 2
+`define VDP_ADDR_SPRITE_BIT_INDICATOR         23
+`define VDP_ADDR_SPRITE_RANGE_NUMBER      8 :  6
+`define VDP_ADDR_SPRITE_BIT_COORDINATES        5
+`define VDP_ADDR_SPRITE_RANGE_ROW         4 :  2
 
-//  Sprite line data
+//  Sprite coordinates data
+//
+//  3322 2222 2222 1111 1111 1100 0000 0000
+//  1098 7654 3210 9876 5432 1098 7654 3210
+//
+//  E... .... ..XX XXXX XXXX ..YY YYYY YYYY
+
+`define VDP_SPRITE_COORD_BIT_ENABLE           31
+`define VDP_SPRITE_COORD_RANGE_X         21 : 12
+`define VDP_SPRITE_COORD_RANGE_Y          9 :  0
+
+//  Sprite row data
 //
 //  3322 2222 2222 1111 1111 1100 0000 0000
 //  1098 7654 3210 9876 5432 1098 7654 3210
--- system_rtl/vdp/vdp_sprite.v
+++ system_rtl/vdp/vdp_sprite.v
@@ -1,86 +1,26 @@
-`include "mfp_ahb_lite_matrix_config.vh"
+`include "vdp.vh"
 
 module vdp_sprite
-# (
-    parameter N_VDP_PIPE  = 0,
-              HPOS_WIDTH  = 10,
-              VPOS_WIDTH  = 10,
-
-              // horizontal constants
-
-              H_DISPLAY   = 640,  // horizontal display width
-              H_FRONT     =  16,  // horizontal right border (front porch)
-              H_SYNC      =  96,  // horizontal sync width
-              H_BACK      =  48,  // horizontal left border (back porch)
-
-              // vertical constants
-
-              V_DISPLAY   = 480,  // vertical display height
-              V_BOTTOM    =  10,  // vertical bottom border
-              V_SYNC      =   2,  // vertical sync # lines
-              V_TOP       =  33   // vertical top border
-)
 (
-    input                         clk,
-    input                         reset,
-    output reg                    hsync,
-    output reg                    vsync,
-    output reg                    display_on,
-    output reg [HPOS_WIDTH - 1:0] hpos,
-    output reg [VPOS_WIDTH - 1:0] vpos
+    input             clk,
+    input             reset,
+
+    input [HPOS_WIDTH - 1:0] hpos,
+    input [VPOS_WIDTH - 1:0] vpos,
+
+    input      [31:0] wr_data,
+    input      [ 2:0] wr_row,
+    input             wr_row_we,
+    input             wr_xy_we,
+
+    output reg        rgb_en,
+    output reg [ 1:0] rgb
 );
 
-    // derived constants
-
-    localparam H_SYNC_START  = H_DISPLAY    + H_FRONT + N_VDP_PIPE,
-               H_SYNC_END    = H_SYNC_START + H_SYNC - 1,
-               H_MAX         = H_SYNC_END   + H_BACK,
-
-               V_SYNC_START  = V_DISPLAY    + V_BOTTOM,
-               V_SYNC_END    = V_SYNC_START + V_SYNC - 1,
-               V_MAX         = V_SYNC_END   + V_TOP;
-
-    // calculating next values of the counters
-
-    reg [HPOS_WIDTH - 1:0] d_hpos;
-    reg [VPOS_WIDTH - 1:0] d_vpos;
-
-    always @*
-    begin
-        if (hpos == H_MAX)
-        begin
-            d_hpos = 1'd0;
-
-            if (vpos == V_MAX)
-                d_vpos = 1'd0;
-            else
-                d_vpos = vpos + 1'd1;
-        end
-        else
-        begin
-          d_hpos = hpos + 1'd1;
-          d_vpos = vpos;
-        end
-    end
-
-    `ifdef MFP_USE_SLOW_CLOCK_AND_CLOCK_MUX
-
-         wire clk_en = 1'b1;
-         
-    `else
-
-        // enable to divide clock from 50 MHz to 25 MHz
-
-        reg clk_en;
-
-        always @ (posedge clk or posedge reset)
-            if (reset)
-                clk_en <= 1'b0;
-            else
-                clk_en <= ~ clk_en;
-    `endif
-
-    // making all outputs registered
+    reg [HPOS_WIDTH - 1:0] x;
+    reg [HPOS_WIDTH - 1:0] y;
+    
+    reg [31:0] rows [0:7];
 
     always @ (posedge clk or posedge reset)
     begin
